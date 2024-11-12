1f485ab2c6e2218827c2f76af8cbe1d58904d8dd
Max Takuro <maxsmurffy@gmail.com>
2024-11-12 23:29:19 +0300

    init

---
+++ cmd/pfm/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	//TODO: init logger
+
+	//TODO: init config
+
+	//TODO: init storage
+
+	//TODO: routes
+
+	//TODO: run
+
+	//TODO: graceful shutdown
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TakuroBreath/personal-finance-manager
+
+go 1.23.3
